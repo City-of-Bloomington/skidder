7d85ccb5097f3a0c5017f70ba7a4a6c5942f5f58
inghamn <inghamn@ccbad9a0-2539-11df-9f67-6fc5ad5a3b7e>
2012-01-18 17:02:47 +0000

    Fixed error: SystemUser cannot require getAuthenticationMethods() function
    git-svn-id: https://skidder.googlecode.com/svn/trunk@16 ccbad9a0-2539-11df-9f67-6fc5ad5a3b7e

--- libraries/framework/classes/SystemUser.php
+++ libraries/framework/classes/SystemUser.php
@@ -18,7 +18,6 @@ abstract class SystemUser extends ActiveRecord
 	abstract public function getRoles();
 
 	abstract public function hasRole($roles);
-	abstract public static function getAuthenticationMethods();
 
 	abstract public function setAuthenticationMethod($method);
 	abstract public function setRoles($roles);
